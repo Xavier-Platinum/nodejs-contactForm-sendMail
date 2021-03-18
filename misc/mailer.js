9c550f37c71f46c95eee4dd70f09683ee735bd80
Xavier-Platinum <kwislawrencekwis@gmail.com>
2021-03-18 06:12:52 +0100

    ...

--- misc/mailer.js
+++ misc/mailer.js
@@ -4,8 +4,8 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-        user: "drrenimusic@gmail.com",
-        pass: "@ccesscontrol00"
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
     }
 });
 
