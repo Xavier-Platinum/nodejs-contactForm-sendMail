4f0b7b6c8eb9e11ec01bd4ec981916c3c54fe445
Xavier-Platinum <kwislawrencekwis@gmail.com>
2021-03-18 06:20:49 +0100

    >>>

--- misc/mailer.js
+++ misc/mailer.js
@@ -16,10 +16,17 @@ let mailOptions = {
     text: "If you get this message then it works",
 }
 
-transporter.sendMail(mailOptions, (err, data) => {
-    if (err) {
-        console.log("An error occured", err);
-    } else {
-        console.log("Email Sent!!!!!")
-    }
-});
+// transporter.sendMail(mailOptions, (err, data) => {
+//     if (err) {
+//         console.log("An error occured", err);
+//     } else {
+//         console.log("Email Sent!!!!!")
+//     }
+// });
+
+// promise
+transporter.sendMail(mailOptions)
+.then((response) => {
+    console.log("Email sent");
+})
+.catch((err) => console.log("Erro:", err));
