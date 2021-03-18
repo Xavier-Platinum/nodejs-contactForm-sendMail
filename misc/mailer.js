3e9c2b9c1d83c46e2d15d18756c8b52092cfc212
Xavier-Platinum <kwislawrencekwis@gmail.com>
2021-03-18 05:44:42 +0100

    updates

--- misc/mailer.js
+++ misc/mailer.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
@@ -9,5 +10,16 @@ const transporter = nodemailer.createTransport({
 });
 
 let mailOptions = {
-    
+    from: "choji@gmail.com",
+    to: "kwislawrencekwis@gmail.com",
+    subject: "Hi!! Just testing",
+    test: "If you get this message then it works",
 }
+
+transporter.sendMail(mailOptions, (err, data) => {
+    if (err) {
+        console.log("An error occured");
+    } else {
+        console.log("Email Sent!!!!!")
+    }
+});
